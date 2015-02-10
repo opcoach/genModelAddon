43bf5c9e2fdf51383b10fb7cfc74116c3dbed70e
Olivier Prouvost <olivier.prouvost@opcoach.com>
2015-02-10 18:24:08 +0100

    Display the files to be overriden and display a dialog box to select them.

--- com.opcoach.genmodeladdon.ui/src/com/opcoach/genmodeladdon/handlers/GeneratedDerivedSourceFolder.java
+++ com.opcoach.genmodeladdon.ui/src/com/opcoach/genmodeladdon/handlers/GeneratedDerivedSourceFolder.java
@@ -2,11 +2,16 @@ package com.opcoach.genmodeladdon.handlers;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Map;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.ILog;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.codegen.ecore.genmodel.GenModel;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.MessageDialog;
@@ -15,8 +20,11 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
 
 import com.opcoach.genmodeladdon.core.GenerateDevStructure;
+import com.opcoach.genmodeladdon.ui.dialog.ConfirmFileSelectionDialog;
 import com.opcoach.genmodeladdon.ui.dialog.DerivedSourceParametersDialog;
 
 public class GeneratedDerivedSourceFolder extends AbstractHandler
@@ -50,15 +58,19 @@ public class GeneratedDerivedSourceFolder extends AbstractHandler
 					gds.generateDevStructure();
 
 					Map<String, Object> filesNotGenerated = gds.filesNotGenerated;
+					StringBuffer filesInError = new StringBuffer();
+
 					if (!filesNotGenerated.isEmpty())
 					{
-						// Must ask if the not generated files can override
-						// existing files...
-						for (String fn : filesNotGenerated.keySet())
+						ConfirmFileSelectionDialog cfs = new ConfirmFileSelectionDialog(parentShell, filesNotGenerated,
+								src);
+						int confirmCode = cfs.open();
+
+						if (confirmCode == ConfirmFileSelectionDialog.OK)
 						{
-							boolean confirm = MessageDialog.openConfirm(parentShell, "Override existing file",
-									"The file " + fn + " already exists. \n\nDo you really want to override it ? ");
-							if (confirm)
+							// Must ask if the not generated files can override
+							// existing files...
+							for (String fn : cfs.getFilesToBeGenerated())
 							{
 								// Open the file and generate contents
 								try
@@ -69,19 +81,40 @@ public class GeneratedDerivedSourceFolder extends AbstractHandler
 									fw.close();
 								} catch (IOException e)
 								{
-									// TODO Auto-generated catch block
-									e.printStackTrace();
+									Bundle bundle = FrameworkUtil.getBundle(this.getClass());
+									ILog log = Platform.getLog(bundle);
+									String mess = "Unable to generate the file : " + fn;
+									Status st = new Status(Status.WARNING, bundle.getSymbolicName(), Status.OK, mess, e);
+									log.log(st);
+									int pos = fn.indexOf(src);
+									filesInError.append(fn.substring(pos)).append("\n");
 								}
+
 							}
 						}
 					}
-					
-					// Display a dialog to explain where files have been generated.
-					MessageDialog.openInformation(parentShell, "Files generated", "Files have been generated in this directory : \n\n" + gds.getSrcAbsolutePath());
+
+					// Display a sum up dialog
+					if (filesInError.length() == 0)
+						MessageDialog.openInformation(parentShell, "Files generated",
+								"Files have been generated in this directory : \n\n" + gds.getSrcAbsolutePath());
+					else
+					{
+						MessageDialog.openWarning(
+								parentShell,
+								"Files generated with errors",
+								"Some errors occured during the generation.\n\n These files were not generated : \n"
+										+ filesInError
+										+ "\n\nThe other files have been generated without any error here : \n"
+										+ gds.getSrcAbsolutePath() + "\n\n---------------------------------------------------\nCheck the logs in : "
+										+ Platform.getLogFileLocation().toPortableString());
+					}
+
 				}
 			}
 		}
 
 		return null;
 	}
+
 }
---
+++ com.opcoach.genmodeladdon.ui/src/com/opcoach/genmodeladdon/ui/dialog/ConfirmFileSelectionDialog.java
@@ -0,0 +1,165 @@
+package com.opcoach.genmodeladdon.ui.dialog;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.resource.ImageRegistry;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.CheckStateChangedEvent;
+import org.eclipse.jface.viewers.CheckboxTableViewer;
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jface.viewers.ColumnViewerToolTipSupport;
+import org.eclipse.jface.viewers.ICheckStateListener;
+import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.jface.window.ToolTip;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Table;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+
+/** A specific dialog to select the files that should be overriden */
+public class ConfirmFileSelectionDialog extends MessageDialog
+{
+	private Map<String, Object> filesNotYetGenerated;
+	private Collection<String> filesToBeGenerated;
+	private CheckboxTableViewer tv;
+	private String relativeDir;
+	private ImageRegistry registry;
+
+	private static final String IMG_CHECKBOX_SELECTED = "icons/checkbox_selected.gif";
+	private static final String IMG_CHECKBOX_UNSELECTED = "icons/checkbox_unselected.gif";
+
+	public ConfirmFileSelectionDialog(Shell parentShell, Map<String, Object> filesNotGenerated, String pRelativeDir)
+	{
+		super(
+				parentShell,
+				"Overriding existing file(s)",
+				null,
+				"Some files already exist.\nSelect the files you want to override.\nA tooltip will display the content of the file if it is kept for generation",
+				MessageDialog.CONFIRM, new String[] { IDialogConstants.OK_LABEL, IDialogConstants.CANCEL_LABEL }, 0);
+		filesNotYetGenerated = filesNotGenerated;
+		relativeDir = pRelativeDir + File.separator;
+		filesToBeGenerated = new ArrayList<String>();
+
+	}
+
+	@Override
+	protected Control createCustomArea(Composite parent)
+	{
+		Composite root = new Composite(parent, SWT.NONE);
+		root.setLayout(new GridLayout(2, false));
+
+		tv = CheckboxTableViewer.newCheckList(root, SWT.NONE);
+		final Table cTable = tv.getTable();
+		cTable.setLinesVisible(true);
+		GridData gd_cTable = new GridData(SWT.FILL, SWT.TOP, true, true);
+		cTable.setLayoutData(gd_cTable);
+
+		TableViewerColumn filenameCol = new TableViewerColumn(tv, SWT.LEAD);
+		filenameCol.getColumn().setWidth(400);
+		filenameCol.getColumn().setText("Filename");
+		filenameCol.setLabelProvider(new ColumnLabelProvider()
+			{
+				@Override
+				public String getText(Object element)
+				{
+					String absName = element.toString();
+					int pos = absName.indexOf(relativeDir);
+					return element.toString().substring(pos);
+				}
+
+				@Override
+				public String getToolTipText(Object element)
+				{
+					return "This content will be generated: \n-----------------------------------\n"
+							+ filesNotYetGenerated.get(element);
+				}
+			});
+		ColumnViewerToolTipSupport.enableFor(tv, ToolTip.NO_RECREATE);
+
+		tv.setContentProvider(ArrayContentProvider.getInstance());
+		tv.setInput(filesNotYetGenerated.keySet());
+
+		tv.addCheckStateListener(new ICheckStateListener()
+			{
+
+				@Override
+				public void checkStateChanged(CheckStateChangedEvent event)
+				{
+					if (event.getChecked())
+					{
+						filesToBeGenerated.add((String) event.getElement());
+					} else
+					{
+						filesToBeGenerated.remove(event.getElement());
+					}
+				}
+			});
+
+		Composite selectComposite = new Composite(root, SWT.NONE);
+		selectComposite.setLayout(new GridLayout(1, false));
+
+		Button selectAll = new Button(selectComposite, SWT.PUSH);
+		selectAll.setImage(getLocalImage(IMG_CHECKBOX_SELECTED));
+		selectAll.setToolTipText("Select all");
+		selectAll.addSelectionListener(new SelectionAdapter()
+			{
+				@Override
+				public void widgetSelected(SelectionEvent e)
+				{
+					tv.setAllChecked(true);
+					filesToBeGenerated.addAll(filesNotYetGenerated.keySet());
+				}
+			});
+
+		Button deselectAll = new Button(selectComposite, SWT.PUSH);
+		deselectAll.setImage(getLocalImage(IMG_CHECKBOX_UNSELECTED));
+		selectAll.setToolTipText("Unselect all");
+		deselectAll.addSelectionListener(new SelectionAdapter()
+			{
+				@Override
+				public void widgetSelected(SelectionEvent e)
+				{
+					tv.setAllChecked(false);
+					filesToBeGenerated.clear();
+
+				}
+			});
+
+		return root;
+	}
+
+	private Image getLocalImage(String key)
+	{
+		if (registry == null)
+		{
+			Bundle b = FrameworkUtil.getBundle(getClass());
+			registry = new ImageRegistry();
+			registry.put(IMG_CHECKBOX_SELECTED, ImageDescriptor.createFromURL(b.getEntry(IMG_CHECKBOX_SELECTED)));
+			registry.put(IMG_CHECKBOX_UNSELECTED, ImageDescriptor.createFromURL(b.getEntry(IMG_CHECKBOX_UNSELECTED)));
+		}
+
+		return registry.get(key);
+	}
+	
+	public Collection<String> getFilesToBeGenerated()
+	{
+		return filesToBeGenerated;
+	}
+
+
+}
