7e4a46583cf9cd4fe0059bdb667c90a626ba074d
Olivier Prouvost <olivier.prouvost@opcoach.com>
2015-05-16 17:39:50 +0200

    First step to fix issue16 and issue15 Convert Handlers to @Execute and add a fragment with handler, command and menucontribution
    Change-Id: I0ecfaf2de58a66d30eb1103d145108befbe7bb98
    Signed-off-by: Olivier Prouvost <olivier.prouvost@opcoach.com>

--- com.opcoach.genmodeladdon.ui/src/com/opcoach/genmodeladdon/handlers/GenerateAntFileHandler.java
+++ com.opcoach.genmodeladdon.ui/src/com/opcoach/genmodeladdon/handlers/GenerateAntFileHandler.java
@@ -4,105 +4,88 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-import org.eclipse.core.commands.AbstractHandler;
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
+import javax.inject.Named;
+
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.emf.codegen.ecore.genmodel.GenModel;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.handlers.HandlerUtil;
 
 import com.opcoach.genmodeladdon.core.GenerateAntFileForCodeGeneration;
 
-public class GenerateAntFileHandler extends AbstractHandler
+public class GenerateAntFileHandler extends GenerateParentHandler
 {
 
 	private final static String ANT_FILENAME = "generateEMFCode.xml";
 
+	@Execute
 	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException
+	public void execute(@Named(IServiceConstants.ACTIVE_SHELL) Shell parentShell,
+			@Named(IServiceConstants.ACTIVE_SELECTION) GenModel gm)
 	{
-		Shell parentShell = HandlerUtil.getActiveShell(event);
-		ISelection sel = HandlerUtil.getCurrentSelection(event);
-		if (sel instanceof IStructuredSelection)
+		File f = getAntFile(gm);
+		boolean createFile = true;
+		if (f.exists())
 		{
-			Object selected = ((IStructuredSelection) sel).getFirstElement();
-			if (selected instanceof GenModel)
+			createFile = MessageDialog.openConfirm(parentShell, "This ant file exists",
+					"Do you really want to override this file " + ANT_FILENAME + " ?");
+		}
+
+		if (createFile)
+		{
+
+			GenerateAntFileForCodeGeneration gen = new GenerateAntFileForCodeGeneration();
+			String s = gm.eResource().toString();
+			int pos = s.lastIndexOf(File.separator);
+			String modelName = s.substring(pos + 1);
+			pos = modelName.indexOf(".genmodel");
+			modelName = modelName.substring(0, pos);
+
+			try
 			{
-				GenModel gm = (GenModel) selected;
+				if (!f.exists())
+					f.createNewFile();
+				FileWriter fw = new FileWriter(f);
+				fw.write(gen.generateAntFileContent(modelName).toString());
+				fw.flush();
+				fw.close();
 
-				File f = getAntFile(gm);
-				boolean createFile = true;
-				if (f.exists())
-				{
-					createFile = MessageDialog.openConfirm(parentShell, "This ant file exists",
-							"Do you really want to override this file " + ANT_FILENAME + " ?");
-				}
+				// Add a refresh
+				IProject proj = ResourcesPlugin.getWorkspace().getRoot().getProject(getProjectName(gm));
 
-				if (createFile)
-				{
+				proj.refreshLocal(IResource.DEPTH_ONE, null);
 
-					GenerateAntFileForCodeGeneration gen = new GenerateAntFileForCodeGeneration();
-					String s = gm.eResource().toString();
-					int pos = s.lastIndexOf(File.separator);
-					String modelName = s.substring(pos + 1);
-					pos = modelName.indexOf(".genmodel");
-					modelName = modelName.substring(0, pos);
-
-					try
-					{
-						if (!f.exists())
-							f.createNewFile();
-						FileWriter fw = new FileWriter(f);
-						fw.write(gen.generateAntFileContent(modelName).toString());
-						fw.flush();
-						fw.close();
-
-						// Add a refresh
-						IProject proj = ResourcesPlugin.getWorkspace().getRoot().getProject(extractProjectName(gm));
-
-						proj.refreshLocal(IResource.DEPTH_ONE, null);
-
-					} catch (IOException e)
-					{
-						MessageDialog.openWarning(parentShell, "Ant file could not be generated",
-								"It was not possible to generate the file : " + ANT_FILENAME
-										+ "\n\n The reason is : \n\n" + e.getMessage());
-					} catch (CoreException e)
-					{
-						MessageDialog.openWarning(parentShell, "Could not refresh the workspace",
-								"It was not possible refresh the workspace. You should do it by hand");
-					}
-				}
-
+			} catch (IOException e)
+			{
+				MessageDialog.openWarning(
+						parentShell,
+						"Ant file could not be generated",
+						"It was not possible to generate the file : " + ANT_FILENAME + "\n\n The reason is : \n\n"
+								+ e.getMessage());
+			} catch (CoreException e)
+			{
+				MessageDialog.openWarning(parentShell, "Could not refresh the workspace",
+						"It was not possible refresh the workspace. You should do it by hand");
 			}
 		}
-		return null;
 
 	}
 
 	private File getAntFile(GenModel gm)
 	{
 		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-		IProject proj = root.getProject(extractProjectName(gm));
+		IProject proj = root.getProject(getProjectName(gm));
 		String srcAbsolutePath = proj.getLocation().toOSString() + File.separator + ANT_FILENAME;
 		File f = new File(srcAbsolutePath);
 		return f;
 	}
 
-	static String extractProjectName(GenModel gm)
-	{
-		String genModelUri = gm.eResource().getURI().toString();
-		String nameStartingWithProjectName = genModelUri.replace("platform:/resource/", "");
-		int pos = nameStartingWithProjectName.indexOf("/");
-		return nameStartingWithProjectName.substring(0, pos);
-	}
+
 
 }
--- com.opcoach.genmodeladdon.ui/src/com/opcoach/genmodeladdon/handlers/GeneratedDerivedSourceFolder.java
+++ com.opcoach.genmodeladdon.ui/src/com/opcoach/genmodeladdon/handlers/GeneratedDerivedSourceFolder.java
@@ -5,29 +5,24 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.eclipse.core.commands.AbstractHandler;
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
+import javax.inject.Named;
+
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.ILog;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.emf.codegen.ecore.genmodel.GenModel;
-import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.handlers.HandlerUtil;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.FrameworkUtil;
 
@@ -36,126 +31,116 @@ import com.opcoach.genmodeladdon.core.GenerateDevStructure;
 import com.opcoach.genmodeladdon.ui.dialog.ConfirmFileSelectionDialog;
 import com.opcoach.genmodeladdon.ui.dialog.DerivedSourceParametersDialog;
 
-public class GeneratedDerivedSourceFolder extends AbstractHandler
+public class GeneratedDerivedSourceFolder extends GenerateParentHandler
 {
 
 	private Shell parentShell;
 
-	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException
+	
+	@Execute
+	public void execute(@Named(IServiceConstants.ACTIVE_SHELL) Shell shell,
+			@Named(IServiceConstants.ACTIVE_SELECTION) GenModel gm)
 	{
+		parentShell = shell;
 
-		parentShell = HandlerUtil.getActiveShell(event);
-		ISelection sel = HandlerUtil.getCurrentSelection(event);
-		if (sel instanceof IStructuredSelection)
+		Shell s = new Shell(parentShell, SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL);
+		s.setText("Enter the parameter for the development structure generation");
+		DerivedSourceParametersDialog dial = new DerivedSourceParametersDialog(s);
+		dial.setGenModel(gm);
+
+		int status = dial.open();
+		if (status == Dialog.OK)
 		{
-			Object selected = ((IStructuredSelection) sel).getFirstElement();
-			if (selected instanceof GenModel)
+			String ip = dial.getDevInterfacePattern();
+			String cp = dial.getDevClassPattern();
+			String src = dial.getSrcDir();
+
+			// Check the genModel dynamic templates.
+			if (!checkGenModelTemplates(gm))
+				return ;
+
+			// Try to generate to check the files that could be
+			// overridden
+			GenerateDevStructure gds = new GenerateDevStructure(gm, cp, ip, src);
+			gds.generateDevStructure(false);
+
+			Map<String, Object> filesNotGenerated = gds.filesNotGenerated;
+			StringBuffer filesInError = new StringBuffer();
+
+			int confirmCode = ConfirmFileSelectionDialog.OK;
+			ConfirmFileSelectionDialog cfs = null;
+
+			if (!filesNotGenerated.isEmpty())
 			{
-				GenModel gm = (GenModel) selected;
-				Shell s = new Shell(parentShell, SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL);
-				s.setText("Enter the parameter for the development structure generation");
-				DerivedSourceParametersDialog dial = new DerivedSourceParametersDialog(s);
-				dial.setGenModel(gm);
+				cfs = new ConfirmFileSelectionDialog(parentShell, filesNotGenerated, src);
+				confirmCode = cfs.open();
+			}
 
-				int status = dial.open();
-				if (status == Dialog.OK)
+			if (confirmCode == ConfirmFileSelectionDialog.OK)
+			{
+				// Now can generate really the files.
+				gds.generateDevStructure(true);
+
+				// Must ask if the not generated files can override
+				// existing files...
+				if (cfs != null)
 				{
-					String ip = dial.getDevInterfacePattern();
-					String cp = dial.getDevClassPattern();
-					String src = dial.getSrcDir();
-
-					// Check the genModel dynamic templates.
-					if (!checkGenModelTemplates(gm))
-						return null;
-
-					// Try to generate to check the files that could be
-					// overridden
-					GenerateDevStructure gds = new GenerateDevStructure(gm, cp, ip, src);
-					gds.generateDevStructure(false);
-
-					Map<String, Object> filesNotGenerated = gds.filesNotGenerated;
-					StringBuffer filesInError = new StringBuffer();
-
-					int confirmCode = ConfirmFileSelectionDialog.OK;
-					ConfirmFileSelectionDialog cfs = null;
-
-					if (!filesNotGenerated.isEmpty())
+					for (String fn : cfs.getFilesToBeGenerated())
 					{
-						cfs = new ConfirmFileSelectionDialog(parentShell, filesNotGenerated, src);
-						confirmCode = cfs.open();
-					}
-
-					if (confirmCode == ConfirmFileSelectionDialog.OK)
-					{
-						// Now can generate really the files.
-						gds.generateDevStructure(true);
-
-						// Must ask if the not generated files can override
-						// existing files...
-						if (cfs != null)
-						{
-							for (String fn : cfs.getFilesToBeGenerated())
-							{
-								// Open the file and generate contents
-								try
-								{
-									FileWriter fw = new FileWriter(fn);
-									fw.write(filesNotGenerated.get(fn).toString());
-									fw.flush();
-									fw.close();
-								} catch (IOException e)
-								{
-									Bundle bundle = FrameworkUtil.getBundle(this.getClass());
-									ILog log = Platform.getLog(bundle);
-									String mess = "Unable to generate the file : " + fn;
-									Status st = new Status(Status.WARNING, bundle.getSymbolicName(), Status.OK, mess, e);
-									log.log(st);
-									int pos = fn.indexOf(src);
-									filesInError.append(fn.substring(pos)).append("\n");
-								}
-
-							}
-						}
-
-						// Refresh the workspace.
-						IProject proj = ResourcesPlugin.getWorkspace().getRoot()
-								.getProject(GenerateAntFileHandler.extractProjectName(gm));
-						// Extract EMF templates to modify the way to inherit
-						// from ancestor
-						EMFPatternExtractor extractor = new EMFPatternExtractor(proj, cp, ip);
-						extractor.run();
+						// Open the file and generate contents
 						try
 						{
-							proj.refreshLocal(IResource.DEPTH_INFINITE, null);
-						} catch (CoreException e)
+							FileWriter fw = new FileWriter(fn);
+							fw.write(filesNotGenerated.get(fn).toString());
+							fw.flush();
+							fw.close();
+						} catch (IOException e)
 						{
+							Bundle bundle = FrameworkUtil.getBundle(this.getClass());
+							ILog log = Platform.getLog(bundle);
+							String mess = "Unable to generate the file : " + fn;
+							Status st = new Status(Status.WARNING, bundle.getSymbolicName(), Status.OK, mess, e);
+							log.log(st);
+							int pos = fn.indexOf(src);
+							filesInError.append(fn.substring(pos)).append("\n");
 						}
 
-						// Display a sum up dialog
-						if (filesInError.length() == 0)
-							MessageDialog.openInformation(parentShell, "Files generated",
-									"Files have been generated in this directory : \n\n" + gds.getSrcAbsolutePath());
-						else
-						{
-							MessageDialog
-									.openWarning(
-											parentShell,
-											"Files generated with errors",
-											"Some errors occured during the generation.\n\n These files were not generated : \n"
-													+ filesInError
-													+ "\n\nThe other files have been generated without any error here : \n"
-													+ gds.getSrcAbsolutePath()
-													+ "\n\n---------------------------------------------------\nCheck the logs in : "
-													+ Platform.getLogFileLocation().toPortableString());
-						}
 					}
+				}
 
+				// Refresh the workspace.
+				IProject proj = ResourcesPlugin.getWorkspace().getRoot()
+						.getProject(getProjectName(gm));
+				// Extract EMF templates to modify the way to inherit
+				// from ancestor
+				EMFPatternExtractor extractor = new EMFPatternExtractor(proj, cp, ip);
+				extractor.run();
+				try
+				{
+					proj.refreshLocal(IResource.DEPTH_INFINITE, null);
+				} catch (CoreException e)
+				{
+				}
+
+				// Display a sum up dialog
+				if (filesInError.length() == 0)
+					MessageDialog.openInformation(parentShell, "Files generated",
+							"Files have been generated in this directory : \n\n" + gds.getSrcAbsolutePath());
+				else
+				{
+					MessageDialog.openWarning(
+							parentShell,
+							"Files generated with errors",
+							"Some errors occured during the generation.\n\n These files were not generated : \n"
+									+ filesInError
+									+ "\n\nThe other files have been generated without any error here : \n"
+									+ gds.getSrcAbsolutePath()
+									+ "\n\n---------------------------------------------------\nCheck the logs in : "
+									+ Platform.getLogFileLocation().toPortableString());
 				}
 			}
-		}
 
-		return null;
+		}
 	}
 
 	/**
@@ -219,12 +204,5 @@ public class GeneratedDerivedSourceFolder extends AbstractHandler
 
 	}
 
-	/** Extract the project name from the resource of genmodel */
-	private String getProjectName(GenModel gm)
-	{
-		URI genModelUri = gm.eResource().getURI();
-		String p = genModelUri.toString().replaceFirst("platform:/resource/", "");
-		int pos = p.indexOf("/");
-		return p.substring(0, pos);
-	}
+
 }
