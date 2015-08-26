a57d8e3a6e2db680e9650bedaa1bf79b1479e9bc
Olivier Prouvost <olivier.prouvost@opcoach.com>
2015-08-26 11:45:35 +0200

    Migration : rewrite handlers with annotations. Extract a ParentHandler to manage the IStructuredSelection still sent by E3 editors. Update dependencies in Manifest
    Change-Id: I95b334a6afc387518e16a23bd145157703ebfab8
    Signed-off-by: Olivier Prouvost <olivier.prouvost@opcoach.com>

---
+++ com.opcoach.genmodeladdon.ui/src/com/opcoach/genmodeladdon/handlers/GenerateParentHandler.java
@@ -0,0 +1,50 @@
+ 
+package com.opcoach.genmodeladdon.handlers;
+
+import javax.inject.Named;
+
+import org.eclipse.e4.core.di.annotations.CanExecute;
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.emf.codegen.ecore.genmodel.GenModel;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Shell;
+
+public abstract class GenerateParentHandler {
+	
+	/** With compatibility layer, must manage also IStructuredSelection ! */
+	@Execute
+	public void execute(@Named(IServiceConstants.ACTIVE_SHELL) Shell shell,
+			@Named(IServiceConstants.ACTIVE_SELECTION) IStructuredSelection isel)
+	{
+		if (isel.getFirstElement() instanceof GenModel)
+			execute(shell, (GenModel) isel.getFirstElement());
+	}
+	
+	@CanExecute
+	public boolean canExecuteWithISelection(@Named(IServiceConstants.ACTIVE_SELECTION) IStructuredSelection isel)
+	{
+		return isel.getFirstElement() instanceof GenModel;
+	}
+	
+	@CanExecute
+	public boolean canExecuteWithObject(@Named(IServiceConstants.ACTIVE_SELECTION) GenModel gm)
+	{
+		return true;
+	}
+
+	abstract public void execute(@Named(IServiceConstants.ACTIVE_SHELL) Shell shell,
+			@Named(IServiceConstants.ACTIVE_SELECTION) GenModel gm);
+
+	/** Extract the project name from the resource of genmodel */
+	protected  String getProjectName(GenModel gm)
+	{
+		URI genModelUri = gm.eResource().getURI();
+		String p = genModelUri.toString().replaceFirst("platform:/resource/", "");
+		int pos = p.indexOf("/");
+		return p.substring(0, pos);
+	}
+	
+
+}
